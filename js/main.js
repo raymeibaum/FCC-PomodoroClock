87e46856bd43495a68ef95be9d6205dd408e3e76
Ray Meibaum <raymeibaum@Rays-iMac.local>
2017-02-28 21:04:57 -0500

    progress bar

--- js/main.js
+++ js/main.js
@@ -1,48 +1,78 @@
 $(function() {
 
-  var model = {
-    checks: 0,
-    progress: {
-      value: 0,
-      color: null,
-      setColor: function(color) {
+  var sessions = 0;
 
-      }
-    },
-    timer: {
-      start: null,
-      end: null,
-      set: function(minutes) {
+  var startTime;
+  var pauseTime;
+  var endTime;
+  var timeRemaining;
+  var intervalID;
+  var progressPercent;
 
-      },
-      start: function() {
 
-      }
+  // Dom elements
+  var $startButton = $('#start-button');
+  var $checkContainer = $('#check-container');
+  var $progressBar = $('#progress-bar');
+
+  $startButton.click(startPomodoro);
+
+  function startPomodoro() {
+    setTimer(10);
+    if (!intervalID) {
+      $startButton.text('Pause');
+      intervalID = window.setInterval(update, 500);
+    } else {
+      window.clearInterval(intervalID);
+      intervalID = null;
+      pauseTimer();
+      $startButton.text('Resume');
     }
   }
-  var controller = {
-    init: function() {
-      view.init();
-    },
-    start: function() {
 
-    }
+  function setTimer(minutes) {
+    startTime = Date.now();
+    endTime = startTime + minutes * 60000;
   }
 
-  var view = {
-    init: function() {
-      this.startButton = $('#start-button');
-      this.startButton.click(controller.start);
-
-      this.progressBar = $('#progress-bar');
-      this.timer = $('#timer');
-
-
-      this.render()
-    },
-    render: function() {
-
-    }
+  function update() {
+    timeRemaining = new Date(endTime - Date.now());
+    progressPercent = Math.floor((Date.now() - startTime) / (endTime - startTime) * 100);
+    $progressBar.attr('style', `width: ${progressPercent}%`);
   }
-  controller.init();
-});
+
+  function pauseTimer() {
+    pauseTime = timeRemaining;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
